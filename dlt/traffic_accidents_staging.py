26cebfb75a757b3db69a6e14c2aee52d73237d82
Daniel Sjoholm <danielblomqvistsjoholm@gmail.com>
2024-09-24 11:55:44 +0200

    Refactor Sort_out_data.ipynb and testconnection.py, and remove unused dbt models and schema definition

--- dlt/traffic_accidents_staging.py
+++ dlt/traffic_accidents_staging.py
@@ -1,76 +1,21 @@
 import dlt
+from pathlib import Path
+import os
+from helpers.trafic import trafikverket_resource
 
-from dlt.sources.helpers.rest_client import paginate
-from dlt.sources.helpers.rest_client.auth import BearerTokenAuth
-from dlt.sources.helpers.rest_client.paginators import HeaderLinkPaginator
-
-# This is a generic pipeline example and demonstrates
-# how to use the dlt REST client for extracting data from APIs.
-# It showcases the use of authentication via bearer tokens and pagination.
-
-
-@dlt.source
-def traffic_accidents_staging_source(api_secret_key: str = dlt.secrets.value):
-    # print(f"api_secret_key={api_secret_key}")
-    return traffic_accidents_staging_resource(api_secret_key)
-
-
-@dlt.resource(write_disposition="append")
-def traffic_accidents_staging_resource(
-    api_secret_key: str = dlt.secrets.value,
-    org: str = "dlt-hub",
-    repository: str = "dlt",
-):
-    # this is the test data for loading validation, delete it once you yield actual data
-    yield [
-        {
-            "id": 1,
-            "node_id": "MDU6SXNzdWUx",
-            "number": 1347,
-            "state": "open",
-            "title": "Found a bug",
-            "body": "I'm having a problem with this.",
-            "user": {"login": "octocat", "id": 1},
-            "created_at": "2011-04-22T13:33:48Z",
-            "updated_at": "2011-04-22T13:33:48Z",
-            "repository": {
-                "id": 1296269,
-                "node_id": "MDEwOlJlcG9zaXRvcnkxMjk2MjY5",
-                "name": "Hello-World",
-                "full_name": "octocat/Hello-World",
-            },
-        }
-    ]
-
-    # paginate issues and yield every page
-    # api_url = f"https://api.github.com/repos/{org}/{repository}/issues"
-    # for page in paginate(
-    #     api_url,
-    #     auth=BearerTokenAuth(api_secret_key),
-    #     # Note: for more paginators please see:
-    #     # https://dlthub.com/devel/general-usage/http/rest-client#paginators
-    #     paginator=HeaderLinkPaginator(),
-    # ):
-    #     # print(page)
-    #     yield page
-
-
-if __name__ == "__main__":
-    # specify the pipeline name, destination and dataset name when configuring pipeline,
-    # otherwise the defaults will be used that are derived from the current script name
+def run_pipeline_for_trafikverket(table_name):
     pipeline = dlt.pipeline(
-        pipeline_name='traffic_accidents_staging',
-        destination='snowflake',
-        dataset_name='traffic_accidents_staging_data',
+        pipeline_name="trafikverket_pipeline",
+        destination="snowflake",
+        dataset_name="staging", 
     )
 
-    data = list(traffic_accidents_staging_resource())
+    load_info = pipeline.run(trafikverket_resource(), table_name=table_name)
+    print("Trafikverket pipeline complete:", load_info)
 
-    # print the data yielded from resource
-    print(data)
+if __name__ == "__main__":
+    working_directory = Path(__file__).parent
+    os.chdir(working_directory)
 
-    # run the pipeline with your parameters
-    # load_info = pipeline.run(source())
-
-    # pretty print the information on data that was loaded
-    # print(load_info)
+    trafikverket_table_name = "trafikverket_data"
+    run_pipeline_for_trafikverket(table_name=trafikverket_table_name)
