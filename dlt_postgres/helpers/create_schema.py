96a2316678540d36c5c3e33dc4731ab1765f8d54
Daniel Sjoholm <danielblomqvistsjoholm@gmail.com>
2024-10-07 13:41:15 +0200

    Refactor code to remove unnecessary schema creation statements

--- dlt_postgres/helpers/create_schema.py
+++
@@ -1,6 +0,0 @@
-def create_schema(conn, cur):
-    """Skapar en tabell som lagrar JSONB-data."""
-    cur.execute("""
-        CREATE SCHEMA IF NOT EXISTS staging;
-    """)
-    conn.commit()
--- dlt_postgres/load_data_postgres_pipeline.py
+++ dlt_postgres/load_data_postgres_pipeline.py
@@ -2,8 +2,6 @@ import dlt
 from pathlib import Path
 import os
 from helpers.trafic import trafikverket_resource
-from helpers.connect_db import connect_db, close_db
-from helpers.create_schema import create_schema
 
 def run_pipeline_for_trafikverket(table_name):
     pipeline = dlt.pipeline(
@@ -14,12 +12,6 @@ def run_pipeline_for_trafikverket(table_name):
 
     load_info = pipeline.run(trafikverket_resource(), table_name=table_name)
     print("Trafikverket pipeline complete:", load_info)
-    try:
-        conn, cur = connect_db()
-        create_schema(conn, cur)
-        close_db(conn, cur)
-    except Exception as e:
-        print(f"Failed to create schema: {e}")
 
 if __name__ == "__main__":
     working_directory = Path(__file__).parent
